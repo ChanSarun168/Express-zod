ba14d118011be27c793d232156a19b8f7d3df2ca
Chan Sarun <chansarun0@gmail.com>
2024-03-18 09:04:19 +0700

    update: zod

--- src/middlewares/userValidation.ts
+++ src/middlewares/userValidation.ts
@@ -1,18 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import z from 'zod';
 import { BaseCustomError } from './validationId';
 
-// Define the Zod schema for user data validation
-const userValidationSchema = z.object({
-  username: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(6),
-});
 
 // Middleware function for validating user data using Zod schema
-export const validateUserData = (req: Request, res: Response, next: NextFunction) => {
+export const validateUserData = (req: Request, res: Response, next: NextFunction , ValidatinSchema:any) => {
   try {
-    userValidationSchema.parse(req.body);
+    ValidatinSchema.parse(req.body);
     next();
   } catch {
     const customError = new BaseCustomError('Wrong Schema', 404);
--- src/routers/users.route.ts
+++ src/routers/users.route.ts
@@ -1,8 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { userController } from "../controllers/userController";
-import { validateUserData } from "../middlewares/userValidation"
-
+import { validateUserData } from "../middlewares/userValidation";
+import userSchema from "../schemas/userSchema";
 
 export const userRouter = express.Router();
 
-userRouter.post('/',validateUserData,userController.createUser);
+userRouter.post(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    validateUserData(req, res, next, userSchema);
+  },
+  userController.createUser
+);
